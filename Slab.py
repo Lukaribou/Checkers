afc8c957a11c4111768b2ac85aa78d428f6791e6
Lukaribou <lucasperroud17@gmail.com>
2020-04-22 19:08:26 +0200

    Update Slab.py
    Reformat position

--- Slab.py
+++ Slab.py
@@ -6,22 +6,22 @@ class Slab:
         """Size: (width, height), position: (line, column), color: ('color_name')"""
         self._window = window
         self._size = (size, size) if isinstance(size, int) else size
-        self._position = position
+        self._position = self.px_position(position)
         self._number = num
         self._color = pygame.Color(color) if isinstance(color, str) else color
         self.draw()
 
-    def px_position(self):
-        return self._position[0] * self._size[0], self._position[1] * self._size[1]
+    def px_position(self, pos):
+        return pos[0] * self._size[0], pos[1] * self._size[1]
 
     def draw(self):
-        pos = self.px_position()
-        pygame.draw.rect(self._window, self._color, (pos[0], pos[1], self._size[0], self._size[1]))
+        pygame.draw.rect(
+            self._window, self._color, (self._position[0], self._position[1], self._size[0], self._size[1]))
 
     def is_mouse_in(self, click):
         """Renvoie True si les coordonnées sont dans la zone du bouton"""
-        coord = self.px_position()
-        return coord[0] < click[0] < (coord[0] + self._size[0]) and coord[1] < click[1] < (coord[1] + self._size[0])
+        return self._position[0] < click[0] < (self._position[0] + self._size[0]) \
+            and self._position[1] < click[1] < (self._position[1] + self._size[0])
 
     def on_clicked(self):
         print("Click", self._number)
